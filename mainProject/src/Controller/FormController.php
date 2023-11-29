d363d5825ef928844d044bb71f5b847bd5ae759a
lalit40070 <luciferlalit112299@gmail.com>
2023-11-29 21:46:18 +0000

    form submit GET 19:24

--- mainProject/src/Controller/FormController.php
+++ mainProject/src/Controller/FormController.php
@@ -9,17 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Post;
 class FormController extends AbstractController
 {
+    
     #[Route('/form', name: 'app_form')]
+    
     public function index(): Response
     {
         
         $post =new Post();
-        // $post->setFirstName('enter first name');
-        // $post->setLastName('enter last name');
-        // $post->setEmail('enter Email id');
-        // $post->setPassword('enter password');
+        
 
-        $form = $this->createForm(PostType::class, $post);
+        $form = $this->createForm(PostType::class, $post, [
+            'action' => $this->generateUrl('form')
+            
+        ]);
+
+
+
+
+
         return $this->render('form/index.html.twig', [
             'post_form' => $form->createView()
         ]);
